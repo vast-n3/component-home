18e8c4a9a5136153b65f6f8ea53dfb12f0aca8a8
neoan <stefan.roehrl@gmail.com>
2020-01-24 17:04:33 -0500

    Setup basic home route

--- Home.ctrl.php
+++ Home.ctrl.php
@@ -4,12 +4,39 @@ namespace Neoan3\Components;
 
 use Neoan3\Frame\Vastn3;
 
+/**
+ * Class Home
+ * @package Neoan3\Components
+ */
 class Home extends Vastn3
 {
+    /**
+     * @var array
+     */
+    private array $vueComponents = [];
+
+    /**
+     * init route
+     */
     function init()
     {
         $this
             ->hook('main', 'home', [])
+            ->addHead('title','Welcome to vast-n3')
+            ->vueComponents($this->vueComponents, [])
             ->output();
+    }
+
+    /**
+     * API test
+     * @param array $parameters
+     * @return array
+     */
+    function getHome($parameters = [])
+    {
+        return [
+            'msg' => 'API response',
+            'sent_params' => $parameters
+        ];
     }
 }
